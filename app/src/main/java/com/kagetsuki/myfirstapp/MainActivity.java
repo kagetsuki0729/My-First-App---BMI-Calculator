a5ece49f05d62b74639557d18fa63934c15bca6f
kagetsuki0729 <ch976317@gmail.com>
2018-01-17 09:42:49 +0800

    [new]connect to server by using socket

--- app/src/main/java/com/kagetsuki/myfirstapp/MainActivity.java
+++ app/src/main/java/com/kagetsuki/myfirstapp/MainActivity.java
@@ -14,12 +14,13 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText edWeight;
     private EditText edHeight;
-    private Button bHelp;
+    private EditText edAge;
+    private EditText edGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(this,"onCreate",Toast.LENGTH_SHORT).show();
+       // Toast.makeText(this,"onCreate",Toast.LENGTH_SHORT).show();
         setContentView(R.layout.activity_main);
         findViews();
         String title = getString(R.string.mytitle);
@@ -29,23 +30,35 @@ public class MainActivity extends AppCompatActivity {
     public void bmi(View v) {
         String w = edWeight.getText().toString();
         String h = edHeight.getText().toString();
+        String a =  edAge.getText().toString();
+        String g =  edGender.getText().toString();
+        if(!g.toLowerCase().equals("male")&&!g.toLowerCase().equals("female")){
+            Toast.makeText(this, "Please enter Male or Female", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         try {
             float weight = Float.parseFloat(w);
             float height = Float.parseFloat(h);
+            int age= Integer.parseInt(a);
             float bmi = weight / (height * height);
             Log.d("BMI", String.valueOf(bmi));
             //Toast.makeText(this, String.valueOf(bmi), Toast.LENGTH_LONG).show();
             //new AlertDialog.Builder(this).setMessage(bmi+" ").setTitle("BMI Result").setPositiveButton("OK",null).show();
             Intent intent = new Intent(this, ResultActivity.class);
             intent.putExtra(getString(R.string.bmi_extra), bmi);
+            intent.putExtra(getString(R.string.age_extra),age);
+            intent.putExtra(getString(R.string.gender_extra),g);
+            /*
             Bundle bag = new Bundle();
             bag.putFloat(getString(R.string.bmi_extra), bmi);
             bag.putString("TEST_EXTRA", "Testing");
-            intent.putExtras(bag);
+            intent.putExtras(bag);*/
             startActivity(intent);
+
         } catch (Exception e) {
             Log.d("Exception ", e.getMessage());
-            Toast.makeText(this, "Please enter valid numbers", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Please enter valid numbers", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -53,48 +66,49 @@ public class MainActivity extends AppCompatActivity {
         //get view elements
         edWeight = (EditText) findViewById(R.id.ed_weight);
         edHeight = (EditText) findViewById(R.id.ed_height);
-        //bHelp = (Button) findViewById(R.id.b_help);
+        edAge=(EditText)findViewById(R.id.ed_age);
+        edGender=(EditText)findViewById(R.id.ed_gender);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Toast.makeText(this,"onStart",Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this,"onStart",Toast.LENGTH_SHORT).show();
         Log.d(" a","onStart");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Toast.makeText(this,"onStop",Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this,"onStop",Toast.LENGTH_SHORT).show();
         Log.d(" a","onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Toast.makeText(this,"onDestroy",Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this,"onDestroy",Toast.LENGTH_SHORT).show();
         Log.d(" a","onDestroy");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Toast.makeText(this,"onPause",Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this,"onPause",Toast.LENGTH_SHORT).show();
         Log.d(" a","onPause");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Toast.makeText(this,"onResume",Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this,"onResume",Toast.LENGTH_SHORT).show();
         Log.d(" a","onResume");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Toast.makeText(this,"onRestart",Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this,"onRestart",Toast.LENGTH_SHORT).show();
         Log.d(" a","onRestart");
     }
 }
--- app/src/main/java/com/kagetsuki/myfirstapp/ResultActivity.java
+++ app/src/main/java/com/kagetsuki/myfirstapp/ResultActivity.java
@@ -1,21 +1,63 @@
 package com.kagetsuki.myfirstapp;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
 
 public class ResultActivity extends AppCompatActivity {
-
+    String bmiResult=new String(" ");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
         Intent intent=getIntent();
-        float bmi=intent.getFloatExtra(getString(R.string.bmi_extra),0);
-        Bundle bag=intent.getExtras();
-        String testing=bag.getString("TEST_EXTRA",null);
-        TextView result=(TextView)findViewById(R.id.result);
-        result.setText("Your BMI is "+bmi+"\n"+testing);
+        final float bmi=intent.getFloatExtra(getString(R.string.bmi_extra),0);
+        final int age=intent.getIntExtra("AGE_EXTRA",0);
+        final String gender=intent.getStringExtra("GENDER_EXTRA");
+        Log.d("Socket","Start a client AsyncTask");
+        AsyncTask<Void,Void,Boolean> client=new AsyncTask<Void,Void,Boolean>(){
+
+            @Override
+            protected Boolean doInBackground(Void... params) {
+                String modifiedSentence;
+                BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
+                try {
+                    Socket clientSocket = new Socket("34.216.142.71", 9999);
+                    PrintStream writer = new PrintStream(clientSocket.getOutputStream());
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    writer.println(bmi+" "+age+" "+gender);
+                    writer.flush();
+                    //System.out.println("Server:" + reader.readLine());
+                    bmiResult+=reader.readLine();
+                    Log.d("Client Result",bmiResult);
+                    clientSocket.close();
+                }
+                catch(Exception e){
+                    Log.d("Socket",e.getMessage().toString());
+                }
+
+                return true;
+            }
+        };
+        try{
+            Boolean atr=client.execute().get();
+            TextView result=(TextView)findViewById(R.id.result);
+            result.setText("Your BMI is "+bmi+"\n"+bmiResult+"\n");
+        }
+        catch(Exception e){
+
+        }
+        //Bundle bag=intent.getExtras();
+        //String testing=bag.getString("TEST_EXTRA",null);
+
     }
 }
+
